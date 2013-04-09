242b60e141c667e9f90ca546df41c79d12aa7456
tomcha <tomcha@MBA.local>
2013-04-09 18:42:30 +0900

    完成

--- myapp.pl
+++ myapp.pl
@@ -1,21 +1,62 @@
 #!/usr/bin/env perl
+use utf8;
 use Mojolicious::Lite;
 
 # Documentation browser under "/perldoc"
-plugin 'PODRenderer';
+#plugin 'PODRenderer';
 
 get '/' => sub {
   my $self = shift;
-  $self->render('index');
+  $self->render('kondo');
 };
+
+post '/' => sub{
+    my $self = shift;
+    my $kantou = $self->param('kantou');
+    my $henkan = $self->param('henkan');
+    my $str;
+    if ($henkan eq '関西風'){
+       if ($kantou eq 'つぎ'){
+           $str = '次発';
+       }else{
+           $str = '先発';
+       };
+    }elsif ($henkan eq 'English'){
+       if ($kantou eq 'つぎ'){
+           $str = 'the second to depart'
+       }else{
+           $str = 'the first to depart'
+       };
+    }else{
+    };
+    $self->stash('str' => $str);
+    $self->render('transration');
+};
+
 
 app->start;
 __DATA__
 
-@@ index.html.ep
+@@ kondo.html.ep
 % layout 'default';
-% title 'Welcome';
-Welcome to the Mojolicious real-time web framework!
+% title '東京の電車';
+%= form_for '/' => method => 'post' => begin
+    %= select_field 'kantou' =>[qw/こんど つぎ/]
+    の電車を
+    %= select_field 'henkan' =>[qw/関西風 English/]
+    で表現すると
+    %= submit_button '変換(Tranceration))'
+% end
+
+@@ transration.html.ep
+% layout 'default';
+% title '東京の電車';
+<%= $str %>
+%= form_for '/' => method => 'get' => begin
+    %= submit_button '戻る'
+% end
+</br>
+このサービスは全て、<a href="http://www.perl-entrance.org/">Perl入学式</a>で学習した内容で作成されています。
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
